5c0a7088b6940d746b74114bf0438e227659cef3
hashir sheikh <hashirsheikh963@gmail.com>
2023-09-14 13:50:54 +0530

    changes for deployment

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nn0t!p)f+pj_4f4vab1q2$yk@-6xt5k0zmku643zkzh-ieuam=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
